3287f7f2f57cb277fc7dd508d88400ce8de4195a
DESKTOP-3DUGTF3\Drew <Mintonat@yahoo.com>
2018-01-21 13:35:43 -0500

    Reset of Project

---
+++ Assets/Scripts/CamraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraFollow : MonoBehaviour {
+
+	public float followHieght = 8f;
+	public float followDistance = 6f;
+
+	private Transform player;
+	private float targerHeight;
+	private float currentHeight;
+	private float currentRotation;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+		targerHeight = player.position.y + followHieght;
+		currentRotation = transform.eulerAngles.y;
+		currentHeight = Mathf.Lerp (transform.position.y, targerHeight, 0.9f * Time.deltaTime);
+		Quaternion euler = Quaternion.Euler (0f, currentRotation, 0f);
+		Vector3 targetPosition = player.position - (euler * Vector3.forward) * followDistance;
+		targetPosition.y = currentHeight;
+		transform.position = targetPosition;
+		transform.LookAt (player);
+
+	}
+}
